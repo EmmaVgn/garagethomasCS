da5748d62ad776f2a8a7bb11db10afe9d4888ebe
EmmaVgn <emma.vigneron@outlook.fr>
2024-10-24 13:14:59 +0200

    Ajout des premiers templates et pages

--- assets/app.js
+++ assets/app.js
@@ -6,5 +6,18 @@ import './bootstrap.js';
  * which should already be in your base.html.twig.
  */
 import './styles/app.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+
+//défilement navbar 
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+      navbar.classList.add('scrolled');
+    } else {
+      navbar.classList.remove('scrolled');
+    }
+  });

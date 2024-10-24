0e25b5eabb4203fa9cc397cd1e2c426d45fc0a92
EmmaVgn <emma.vigneron@outlook.fr>
2024-10-24 13:42:48 +0200

    Mise en place de l'entité User et de la Fixtures Beta

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Faker\Factory;
+use App\Entity\User;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class AppFixtures extends Fixture
+{
+    protected $passwordHasher;
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create('fr_FR');
+        $admin = new User;
+        $hash = $this->passwordHasher->hashPassword($admin, "admin");
+        $admin->setEmail("admin@gmail.fr")
+            ->setFirstname("Vincent")
+            ->setLastname("Parot")
+            ->setPassword($hash)
+            ->setRoles(['ROLE_ADMIN']);
+
+        $manager->persist($admin);
+
+        for ($u = 0; $u < 5; $u++) {
+            $user = new User;
+            $hash = $this->passwordHasher->hashPassword($user, "password");
+            $user->setEmail("user$u@gmail.com")
+                ->setFirstname($faker->firstName())
+                ->setLastname($faker->lastName())
+                ->setPassword($hash);
+            $manager->persist($user);
+        }
+
+        $manager->flush();
+    }
+}
